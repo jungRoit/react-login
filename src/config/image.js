46acc722149359a74630f5e6bd41bcefe3da590e
jungRoit <roit.github@gmail.com>
2020-09-08 20:47:37 +0545

    created formInput component

---
+++ src/config/image.js
@@ -0,0 +1,3 @@
+export const userIcon = require('../assets/images/user.png');
+export const emailIcon = require('../assets/images/email.png');
+export const lockIcon = require('../assets/images/lock.png');
--- src/views/SignUp/signUp.js
+++ src/views/SignUp/signUp.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Button from '../../ components/Button';
+import FormInput from '../../ components/FormInput';
+import {userIcon, emailIcon, lockIcon} from '../../config/image';
+
 
 class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
-    }
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    };
   }
 
-signupHandler = () => {
-  alert('submitted');
-}
+  signupHandler = () => {
+    alert('submitted');
+  };
+
+  handleOnChange = (e) => {
+    this.setState({ [e.target.name]:e.target.value });
+  };
 
   render() {
     return (
@@ -20,34 +30,55 @@ signupHandler = () => {
           <div className="signup-box-header">
             <h1 className="sign-box-header-title">Register with Us</h1>
           </div>
-          <div  className="signup-box-form">
-          <form className="sign-up-form" onSubmit={this.signupHandler}>
-            <div className="form-element">
-              <label className="form-element-label">First Name</label>
-              <input type="text"  className="form-element-submit" />
-            </div>
-            <div className="form-element">
-              <label className="form-element-label">Last Name</label>
-              <input type="text"  className="form-element-submit" />
-            </div>
-            <div className="form-element">
-              <label className="form-element-label">Email</label>
-              <input type="text"  className="form-element-submit" />
-            </div>
-            <div className="form-element">
-              <label className="form-element-label">Password</label>
-              <input type="text"  className="form-element-submit" />
-            </div>
-            <div className="form-buttons">
-            <Button type="submit" text={"Sign up"}></Button>
-            <p>Already have an Account? <a href="#" className="link"> Sign In</a></p>
-            </div>
-          </form>
+          <div className="signup-box-form">
+            <form className="sign-up-form" onSubmit={this.signupHandler}>
+              <FormInput
+                onChange={this.handleOnChange}
+                value={this.state.firstName}
+                label="First Name"
+                type="text"
+                name="firstName"
+                icon={userIcon}
+              />
+              <FormInput
+                onChange={this.handleOnChange}
+                value={this.state.lastName}
+                label="Last Name"
+                type="text"
+                name="lastName"
+                icon={userIcon}
+              />
+              <FormInput
+                onChange={this.handleOnChange}
+                value={this.state.email}
+                label="Email"
+                type="email"
+                name="email"
+                icon={emailIcon}
+              />
+              <FormInput
+                onChange={this.handleOnChange}
+                value={this.state.password}
+                label="Password"
+                type="password"
+                name="password"
+                icon={lockIcon}
+              />
+              <div className="form-buttons">
+                <Button type="submit" text={'Sign up'}></Button>
+                <p>
+                  Already have an Account?{' '}
+                  <a href="#" className="link">
+                    {' '}
+                    Sign In
+                  </a>
+                </p>
+              </div>
+            </form>
           </div>
-          
         </div>
       </div>
-    )
+    );
   }
 }
 
