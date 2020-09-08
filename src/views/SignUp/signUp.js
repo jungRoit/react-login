78366383e75d4d5f5ebc745691ab5cbfbc152120
jungRoit <roit.github@gmail.com>
2020-09-08 10:17:35 +0545

    basic layout completed

--- src/views/SignUp/signUp.js
+++ src/views/SignUp/signUp.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import Button from '../../ components/Button';
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+signupHandler = () => {
+  alert('submitted');
+}
 
   render() {
     return (
-      <div>
-        <h1>Sign up</h1>
+      <div className="container">
+        <div className="sign-up-box">
+          <div className="signup-box-header">
+            <h1 className="sign-box-header-title">Sign Up</h1>
+          </div>
+          <div  className="signup-box-form">
+          <form className="sign-up-form" onSubmit={this.signupHandler}>
+            <div className="form-element">
+              <label className="form-element-label">First Name</label>
+              <input type="text"  className="form-element-submit" />
+            </div>
+            <div className="form-element">
+              <label className="form-element-label">Last Name</label>
+              <input type="text"  className="form-element-submit" />
+            </div>
+            <div className="form-element">
+              <label className="form-element-label">Email</label>
+              <input type="text"  className="form-element-submit" />
+            </div>
+            <div className="form-element">
+              <label className="form-element-label">Password</label>
+              <input type="text"  className="form-element-submit" />
+            </div>
+            <div className="form-buttons">
+            <Button type="submit" text={"Sign up"}></Button>
+            <p>Already have an Account? <a href="#"> Sign In</a></p>
+            </div>
+          </form>
+          </div>
+          
+        </div>
       </div>
     )
   }
