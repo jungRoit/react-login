662490f3f600937d8ca873076d81d83f316786aa
jungRoit <roit.github@gmail.com>
2020-09-08 22:19:38 +0545

    Updates

---
+++ precache-manifest.0ddb31e76a003e7964fbf2a409961d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7dcf2960eff0b06532f11ac8cf1f8f",
+    "url": "/react-login/index.html"
+  },
+  {
+    "revision": "95a1f7b7fbef9ef6e111",
+    "url": "/react-login/static/css/main.0e1945cb.chunk.css"
+  },
+  {
+    "revision": "6b9153e5428f9620226b",
+    "url": "/react-login/static/js/2.7e88fbd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-login/static/js/2.7e88fbd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a1f7b7fbef9ef6e111",
+    "url": "/react-login/static/js/main.955a8a55.chunk.js"
+  },
+  {
+    "revision": "e594bbf9619b4a92055f",
+    "url": "/react-login/static/js/runtime-main.d14630e6.js"
+  }
+]);
