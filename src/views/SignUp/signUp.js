029405e36f5996b09c2e1ef9e306aa0c7b16f506
jungRoit <roit.github@gmail.com>
2020-09-08 22:15:43 +0545

    added error and succes message

--- src/views/SignUp/signUp.js
+++ src/views/SignUp/signUp.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Button from '../../ components/Button';
 import FormInput from '../../ components/FormInput';
-import {userIcon, emailIcon, lockIcon} from '../../config/image';
-import {validatePassword} from '../../utils/validators';
+import { userIcon, emailIcon, lockIcon } from '../../config/image';
+import { validatePassword } from '../../utils/validators';
 import * as ApiService from '../../services/api';
-import {emailValidation, signUp} from '../../config/url';
-
+import { emailValidation, signUp } from '../../config/url';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -15,30 +14,34 @@ class SignUp extends React.Component {
       lastName: '',
       email: '',
       password: '',
-      errorMessage: ''
+      errorMessage: '',
+      success: false,
     };
   }
 
   validateInputs = () => {
-    if(!this.state.email) {
-      this.setState({errorMessage:'Email cannot be empty'});
+    if (!this.state.email) {
+      this.setState({ errorMessage: 'Email cannot be empty', password:'' });
+      this.clearText()
       return false;
     }
 
-    if(!this.state.password) {
-      this.setState({errorMessage:'password cannot be empty'});
+    if (!this.state.password) {
+      this.setState({ errorMessage: 'password cannot be empty', password:'' });
+      this.clearText()
       return false;
     }
     return true;
-  }
+  };
 
-  signupHandler = async e => {
+  signupHandler = async (e) => {
     e.preventDefault();
     const isValid = this.validateInputs();
-    if(isValid) {
+    if (isValid) {
       const passwordValidation = validatePassword(this.state.password);
-      if(!passwordValidation.isValid) {
-        this.setState({errorMessage: passwordValidation.message});
+      if (!passwordValidation.isValid) {
+        this.setState({ errorMessage: passwordValidation.message, password:'' });
+        this.clearText()
         return;
       }
 
@@ -48,12 +51,19 @@ class SignUp extends React.Component {
         const emailValidationBody = {
           campaignUuid,
           data: {
-            email: this.state.email
-          }
-         }
-        const emailValidationResponse = await ApiService.post(emailValidation,emailValidationBody);
-        if(emailValidationResponse.data.data.status === 'EXISTS') {
-          this.setState({errorMessage: 'Email Already exists. Please use a new Email.'});
+            email: this.state.email,
+          },
+        };
+        const emailValidationResponse = await ApiService.post(
+          emailValidation,
+          emailValidationBody
+        );
+        if (emailValidationResponse.data.data.status === 'EXISTS') {
+          this.setState({
+            errorMessage: 'Email Already exists. Please use a new Email.',
+            password:''
+          });
+          this.clearText()
           return;
         }
 
@@ -63,18 +73,34 @@ class SignUp extends React.Component {
             firstName: this.state.firstName,
             lastName: this.state.lastName,
             email: this.state.email,
-            password: this.state.password
-          }
-         }
-        const response = await ApiService.post(signUp,signUpBody);
+            password: this.state.password,
+          },
+        };
+        const response = await ApiService.post(signUp, signUpBody);
+        this.setState({ success: true });
+        this.clearText()
       } catch (error) {
-        this.setState({errorMessage:error});
+        console.log('err', error, error.response);
+        if (error.response.data.errors[0]) {
+          this.setState({ errorMessage: error.response.data.errors[0].detail, password:'' });
+        } else {
+          this.setState({
+            errorMessage: 'Error Registering User. Please try again.',
+            password:''
+          });
+        }
       }
     }
   };
 
+  clearText = () => {
+    setTimeout(()=>{
+      this.setState({errorMessage:'', success:false})
+    },3000)
+  }
+
   handleOnChange = (e) => {
-    this.setState({ [e.target.name]:e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
@@ -119,6 +145,14 @@ class SignUp extends React.Component {
                 icon={lockIcon}
               />
               <div className="form-buttons">
+                {this.state.errorMessage && (
+                  <p className="error-message">{this.state.errorMessage}</p>
+                )}
+                {this.state.success && (
+                  <p className="success-message">
+                    {'Succesfully Registered Account. Thank You for Registering with us.'}
+                  </p>
+                )}
                 <Button type="submit" text={'Sign up'}></Button>
                 <p>
                   Already have an Account?{' '}
